f014545fcb8a9c071f7caa22a842e2da7ed25a2e
AndreAlberto <andregroualberto@hotmail.com>
2025-06-24 15:09:31 +0100

    atualizado JS (IVA 6%)

---
+++ JS/carousel.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const carouselItems = document.querySelectorAll(".carousel-item");
+    let currentIndex = 0;
+
+    function showNextItem() {
+        carouselItems[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex + 1) % carouselItems.length;
+        carouselItems[currentIndex].classList.add("active");
+    }
+
+    carouselItems[currentIndex].classList.add("active");
+    setInterval(showNextItem, 5000);
+});
